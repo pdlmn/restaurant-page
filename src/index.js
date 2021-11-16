a4d7e9fead56d59f247783714ba0d88642bcd272
pdlmn <djeinwings@gmail.com>
2021-11-16 23:24:03 +0200

    Added CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import './style.css';
+import './styles/index.css';
+import './styles/utils.css';
 
 console.log('Everything is working!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -12,6 +16,10 @@ module.exports = {
       {
         test: /\.css/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
